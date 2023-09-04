0e6f3e05188c1c15bd4a526cbe9bfddd190f1bfa
Hunter171 <99055264+Hunter-171@users.noreply.github.com>
2023-09-04 22:28:17 +0200

    Update api.py
    Charlie said so

--- genius_wrapper/genius/api.py
+++ genius_wrapper/genius/api.py
@@ -4,10 +4,7 @@ import requests
 QUERY_URL = "https://genius.com/api/search/multi?q="
 
 class Genius:
-    def __init__(self):
-        pass
-
-    def query(self, query):
+    def perform_query(self, query):
         res = requests.get(QUERY_URL+query)
         query = utils.parse_query(res.json())
         return query
